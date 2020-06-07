35ee3bc916159c514b6bfe42ad459eef62298d31
samline <jsamline@gmail.com>
2020-06-07 15:39:12 -0500

    all in sass, removed colors

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,13 +6,29 @@ mix
 .sourceMaps()
 
 if(mix.inProduction()) {
-  mix.less(
+  /* mix.less(
     'src/less/shido.less',
     'css/shido.min.css'
+  ) */
+  mix.sass(
+    'src/sass/shido.scss',
+    'css/shido.min.css'
+  )
+  mix.sass(
+    'src/sass/shido-colors.scss',
+    'css/shido-colors.min.css'
   )
 } else {
-  mix.less(
+  /* mix.less(
     'src/less/shido.less',
     'css/shido.css'
+  ) */
+  mix.sass(
+    'src/sass/shido.scss',
+    'css/shido.css'
+  )
+  mix.sass(
+    'src/sass/shido-colors.scss',
+    'css/shido-colors.css'
   )
 }
