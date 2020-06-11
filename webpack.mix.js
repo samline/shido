7d19191eda3cda30f60fc6a35f185d0601a5a4db
Samuel Olvera <jsamline@gmail.com>
2020-06-10 20:08:36 -0500

    Revert "Sass"

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,45 +6,13 @@ mix
 .sourceMaps()
 
 if(mix.inProduction()) {
-  mix
-
-  .less(
+  mix.less(
     'src/less/shido.less',
     'css/shido.min.css'
   )
-  .less(
-    'src/less/shido-colors.less',
-    'css/shido-colors.min.css'
-  )
-
-  .sass(
-    'src/sass/shido.scss',
-    'css/shido.min.css'
-  )
-  .sass(
-    'src/sass/shido-colors.scss',
-    'css/shido-colors.min.css'
-  )
-
 } else {
-  mix
-
-  .less(
+  mix.less(
     'src/less/shido.less',
     'css/shido.css'
   )
-  .less(
-    'src/less/shido-colors.less',
-    'css/shido-colors.css'
-  )
-
-  .sass(
-    'src/sass/shido.scss',
-    'css/shido.css'
-  )
-  .sass(
-    'src/sass/shido-colors.scss',
-    'css/shido-colors.css'
-  )
-
 }
